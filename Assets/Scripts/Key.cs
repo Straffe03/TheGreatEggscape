2819b6b72a209317b55c7862d2bf6828bd2d9d93
Straffe <mariolayap@gmail.com>
2025-05-22 15:44:44 +0200

    sonido y particulas añadido a la llave

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -1,13 +1,33 @@
 using System;
 using UnityEngine;
 
+
 public class Key : MonoBehaviour
 {
+    private ParticleSystem collectParticles;
     public Action onCollected;
+    private AudioSource audioSource;
+    void Start()
+    {
+        collectParticles = GetComponentInChildren<ParticleSystem>();
+        audioSource = GetComponentInChildren<AudioSource>();
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            if (collectParticles != null)
+            {
+                collectParticles.transform.parent = null; // lo soltamos para que no muera con la llave
+                collectParticles.Play();
+                Destroy(collectParticles.gameObject, collectParticles.main.duration);
+            }
+            if (audioSource != null && audioSource.clip != null)
+            {
+                audioSource.transform.parent = null;
+                audioSource.PlayOneShot(audioSource.clip);
+                Destroy(audioSource.gameObject, collectParticles.main.duration);
+            }
             GameManager.Instance.KeyCollected();
             onCollected?.Invoke();
             Destroy(gameObject);
